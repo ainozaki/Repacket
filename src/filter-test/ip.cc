de0a564728134eeac2f15c87a453e4f267356b92
ainnoo <aiai10piano@gmail.com>
2021-12-22 02:16:31 +0000

    [FilterTest] add ip_tot_len_max test.

--- src/filter-test/ip.cc
+++ src/filter-test/ip.cc
@@ -253,3 +253,50 @@ TEST(IPTest, FilterByTotLenMin) {
 
   loader.DetachBpf();
 }
+
+TEST(IPTest, FilterByTotLenMax) {
+  config cfg;
+  cfg.mode = Mode::Attach;
+  cfg.xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST;
+  cfg.ifindex = if_nametoindex("veth1");
+  cfg.ifname = "veth1";
+  cfg.bpf_filepath = "filter-test/xdp_test_ip_tot_len_max.o";
+  cfg.progsec = "xdp_generated";
+  Loader loader(cfg.mode, cfg.xdp_flags, cfg.ifindex, cfg.ifname,
+                cfg.bpf_filepath, cfg.progsec);
+  loader.Start();
+
+  system(
+      "/usr/bin/bash /home/vagrant/MocTok/src/filter-test/ip.sh "
+      "ip_tot_len");
+
+  Stats stats("veth1", "filter-test/ip_tot_len_max.yaml");
+  __u32 key = 1;
+  datarec value1 = stats.GetMapValueForTesting(key);
+  key++;
+  datarec value2 = stats.GetMapValueForTesting(key);
+  key++;
+  datarec value3 = stats.GetMapValueForTesting(key);
+  key++;
+  datarec value4 = stats.GetMapValueForTesting(key);
+  key++;
+  datarec value5 = stats.GetMapValueForTesting(key);
+  // fiter1 is ttl<=64.
+  // fiter2 is ttl<=128.
+  // fiter3 is ttl<=512.
+  // fiter4 is ttl<=1024.
+  // fiter5 is ttl<=1500.
+  // In this test,
+  // send 1 packets of tot_len=48.
+  // send 2 packets of tot_len=68.
+  // send 3 packets of tot_len=228.
+  // send 4 packets of tot_len=528.
+  // send 5 packets of tot_len=1028.
+  EXPECT_EQ(1, value1.rx_packets);
+  EXPECT_EQ(2, value2.rx_packets);
+  EXPECT_EQ(3, value3.rx_packets);
+  EXPECT_EQ(4, value4.rx_packets);
+  EXPECT_EQ(5, value5.rx_packets);
+
+  loader.DetachBpf();
+}
