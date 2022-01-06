d81d86bb82ab10331c41569e14f736aa056a96ae
ainnoo <aiai10piano@gmail.com>
2022-01-06 18:44:26 +0900

    [IpAddress] handle ip_address to display.

--- src/core/dump/binary_utils.h
+++ src/core/dump/binary_utils.h
@@ -3,6 +3,7 @@
 
 #include <arpa/inet.h>
 #include <stdint.h>
+#include <string.h>
 
 static inline uint8_t GET_U8(const void *p)
 {
@@ -12,6 +13,18 @@ static inline uint8_t GET_U8(const void *p)
 static inline uint16_t GET_U16(const void *p)
 {
 	return (uint16_t)ntohs(*(const uint16_t *)(p));
+}
+
+static inline char* GET_IPADDR(const void *p)
+{
+	char buf[32];
+	char *ptr;
+	// inet_ntop expects network-order binary.
+	inet_ntop(AF_INET, (struct in_addr *)p, buf, sizeof(buf));
+	ptr = buf;
+	// TODO: buf address is the same for every call. why?
+	// printf("buf ptr: %p\n", buf);
+	return ptr;
 }
 
 #endif // BINARY_UTILS_H_
--- src/core/dump/print_ip.c
+++ src/core/dump/print_ip.c
@@ -9,8 +9,12 @@
 void ip_print(const unsigned char* p, uint8_t len) {
   const struct ip* ip;
   uint8_t proto;
+  char* src;
+  char* dst;
 
   ip = (const struct ip*)p;
   proto = GET_U8(ip->protocol);
-  printf("IP %s", ip_protoname(proto));
+  src = GET_IPADDR(ip->saddr);
+  dst = GET_IPADDR(ip->daddr);
+  printf("IP %s > %s: %s", src, dst, ip_protoname(proto));
 }
